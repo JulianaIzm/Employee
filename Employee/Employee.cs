9bba8a62c5cf53c9c83efe7574649392ef101f1d
Juliana Izmaylova <aeliren12@yandex.ru>
2024-06-06 20:29:14 +0300

    first

---
+++ Employee/Employee.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF3
+{
+    public class Employee
+    {
+        // Поля
+        private string name;
+        private int salary;
+        private DateTime hireDate;
+        public int id;
+        public static int nextId = 0;
+
+        // Конструкторы
+
+        //default
+        public Employee()
+        {
+            name = "Иванов Иван Иванович";
+            salary = 1000;
+            hireDate = DateTime.Now;
+            this.id = getNextId();
+        }
+        // 5 параметров: имя, зарплата, дд, мм,гггг  - трудоустройства.
+        public Employee(string name, int salary, int day, int month, int year)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.hireDate = new DateTime(year, month, day);
+            this.id = getNextId();
+        }
+
+        // 2 параметра: имя, зарплата (дата трудоустройства - время создания объекта).
+        public Employee(string name, int salary) 
+        {
+            this.name = name;
+            this.salary = salary;
+            this.hireDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            this.id = getNextId();
+        }
+
+        // Свойства
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Значение должно быть положительным");
+                }
+                else
+                {
+                    this.salary = value;
+                }
+            }
+        }
+
+        public virtual int getSalary()
+        {
+            return this.salary;
+        }
+
+        public override string ToString()
+        {
+            return this.name +", зарплата = " + this.salary + " дата приема = " + this.hireDate;
+            //return "id: " + id + " сотрудник ФИО: " + this.name + ", зарплата = " + this.salary + ", дата приема на работу: " + this.hireDate;
+        }
+
+        // Метод увеличения зарплаты на заданный процент
+        public void IncreaseSalary(int percent)
+        {
+            this.Salary += this.salary * percent / 100;
+        }
+
+        public int getNextId()
+        {
+            nextId += 1;
+            return nextId;
+        }
+        public int getId()
+        {
+            return this.id;
+        }
+        public void setId()
+        {
+            this.id = getNextId();
+        }
+    }
+}
---
+++ Employee/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF3
+{
+        public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Employee[] emps = new Employee[10];
+        public static int count = 0;
+
+
+        private void addEmployee_Click(object senderic, RoutedEventArgs eventArg)
+        {
+            if (textboxName.Text == "" || textboxSalary.Text == "")
+            {
+                emps[count] = new Employee();
+                listbox.Items.Add(emps[count]);
+                textboxName.Text = textboxSalary.Text = "";
+                datePicker1.SelectedDate = null;
+            }
+
+            else if (datePicker1.SelectedDate == null)
+            {
+                emps[count] = new Employee(textboxName.Text, Convert.ToInt32(textboxSalary.Text));
+                listbox.Items.Add(emps[count]);
+                textboxName.Text = textboxSalary.Text = "";
+                datePicker1.SelectedDate = null;
+            }
+            else
+            {
+                emps[count] = new Employee(textboxName.Text, Convert.ToInt32(textboxSalary.Text), datePicker1.SelectedDate.Value.Day, datePicker1.SelectedDate.Value.Month, datePicker1.SelectedDate.Value.Year);
+                listbox.Items.Add(emps[count]);
+                textboxName.Text = textboxSalary.Text = "";
+                datePicker1.SelectedDate = null;
+            }
+        }
+
+        private void increaseSalary_Click(object a, RoutedEventArgs b)
+        {
+            
+            /*foreach (var employee in emps)
+            {
+                
+                employee.IncreaseSalary(30);
+                listbox.Items.Clear();
+                listbox.Items.Add(employee);
+            }*/
+            //emps[0].IncreaseSalary(20);
+            //listbox.Items.Add(emps[0]);
+        }
+    }
+}
